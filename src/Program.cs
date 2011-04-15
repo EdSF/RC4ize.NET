58b34fc10a544f782ed9843f7bfe15de775a922c
Marcelo Shiniti Uchimura <uchi.m@uol.com.br>
2011-04-15 09:37:38 -0300

    snipped a line from src/Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,6 @@ namespace RC4.NET.ConsoleTest
             byte[] key = Encoding.ASCII.GetBytes("FACAFACAFA");
             byte[] iv = Encoding.ASCII.GetBytes("0102030405");
 
-            RC4Transform t = new RC4Transform(key, iv);
             Encoding enco = Encoding.GetEncoding("iso-8859-1");
 
             Console.WriteLine("Welcome to the RC4.NET Test Console App");
